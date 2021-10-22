42a98554539cc5a61780c45137997a27897998d3
himanibm <71306305+himanibm@users.noreply.github.com>
2021-10-22 15:33:02 +0530

    Add files via upload

---
+++ lab2/LAB2_IOT/LAB2_IOT.ino
@@ -0,0 +1,17 @@
+int buttonPin = 2;
+int ledPin = 13;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(ledPin , OUTPUT);
+pinMode(buttonPin , INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int buttonState = digitalRead(buttonPin);
+if(buttonState == HIGH) {
+ digitalWrite(ledPin, HIGH); 
+}else {
+  digitalWrite(ledPin, LOW);
+}
+}
