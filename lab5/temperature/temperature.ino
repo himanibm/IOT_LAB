6d8626542c683b2b28a54a887e006c2beb984221
himanibm <71306305+himanibm@users.noreply.github.com>
2021-11-12 15:30:51 +0530

    Add files via upload

---
+++ lab5/temperature/temperature.ino
@@ -0,0 +1,17 @@
+int outputPin=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int rawvoltage=analogRead(outputPin);
+float millivolts=(rawvoltage/1024.0)*5000;
+float celsius=millivolts/10;
+Serial.print(celsius);
+Serial.print("degreesCelsius,");
+Serial.print((celsius*9)/5+32);
+Serial.println("degreesFahrenheit");
+delay(1000);
+}
