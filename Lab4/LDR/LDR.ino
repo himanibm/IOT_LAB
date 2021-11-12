66b92a440a02e170f8610949fca9669ebe3c3d02
himanibm <71306305+himanibm@users.noreply.github.com>
2021-11-12 15:18:37 +0530

    Add files via upload

---
+++ Lab4/LDR/LDR.ino
@@ -0,0 +1,21 @@
+int LDR=0;
+int LDRValue=0;
+int light_sensitivity=1000;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(12,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ int LDRValue=analogRead(A0);
+ Serial.println(LDRValue);
+ delay(50);
+ if(LDRValue<light_sensitivity)
+ digitalWrite(12,HIGH);
+ else
+ digitalWrite(12,LOW);
+ delay(100);
+}
+
