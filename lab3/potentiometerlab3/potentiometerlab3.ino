7d30f54185da571949e9483f1d7a151d77ff9a4d
himanibm <71306305+himanibm@users.noreply.github.com>
2021-11-12 14:56:48 +0530

    Add files via upload

---
+++ lab3/potentiometerlab3/potentiometerlab3.ino
@@ -0,0 +1,20 @@
+
+int analogInPin = A0;
+int analogOutPin = 9;
+
+int sensorValue = 0;
+int outputValue = 0;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = analogRead(analogInPin);
+  outputValue = map(sensorValue, 0, 1023, 0, 255);
+  analogWrite(analogOutPin, outputValue);
+  Serial.print(sensorValue);
+  Serial.println(outputValue);
+  delay(1000);
+}
