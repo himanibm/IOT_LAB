0b8a08b9c510c7a598d87543a2283fbb172d46bf
himanibm <71306305+himanibm@users.noreply.github.com>
2021-12-03 15:04:18 +0530

    Add files via upload

---
+++ lab9/piriot.ino
@@ -0,0 +1,26 @@
+int pirpin=2;
+int ledpin=13;
+int ct=30;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(pirpin,INPUT);
+pinMode(ledpin,OUTPUT);
+digitalWrite(pirpin,LOW);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(digitalRead(pirpin)==HIGH)
+{
+  digitalWrite(ledpin,HIGH);
+}
+delay(5000);
+if(digitalRead(pirpin)==LOW)
+{
+  digitalWrite(ledpin,LOW);
+}
+delay(1000);
+}
+
